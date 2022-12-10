dc18d13cb2e0f17f9b175152be0cf80f93dfff24
Prathamesh Rukade <prathameshrukade@gmail.com>
2022-12-10 18:03:57 +0530

    first

--- app/src/main/java/com/example/video/MainActivity.java
+++ app/src/main/java/com/example/video/MainActivity.java
@@ -8,6 +8,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOption;
 
@@ -19,6 +20,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     Button demoBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,26 +47,26 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.codeBox);
         String text = editText.getText().toString();
 
-        startActivity(new Intent(MainActivity.this ,LoginActivity.class));
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
         if (text.length() > 0) {
             MediaStore.Video options
                     = new MediaStore.Video().Builder()
 ///            JitsiMeetConferenceOptions op=new JistiMeetConferenceOptions.Builder()
                     .setRoom(text)
-                    .setFeatureFlag("invite.enabled",false)
-                    .setFeatureFlag("help.enabled",false)
+                    .setFeatureFlag("invite.enabled", false)
+                    .setFeatureFlag("help.enabled", false)
                     .build();
 //            JitsiMeetActivity.launch(this,options);
         }
 
 
-        demoBtn= (Button) findViewById(R.id.demoBtn);
+        demoBtn = (Button) findViewById(R.id.demoBtn);
         demoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
     }
